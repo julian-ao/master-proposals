bc5326584463bce5b8555596aba646aef0f47d9c
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2025-03-24 12:50:08 +0100

    init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://www.idi.ntnu.no',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/education'),
+        secure: false,
+      }
+    }
+  }
+})

03579191478e3b4f3b991fd3bdc8b853d98afb28
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2025-03-24 13:17:54 +0100

    cors fix?

---
+++ middleware.js
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+    const response = NextResponse.next();
+
+    // Add CORS headers
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+
+    return response;
+}

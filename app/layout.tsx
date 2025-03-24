fb4b911279bd24c88d79369cb294774729bcb0ef
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2025-03-24 20:16:59 +0100

    analytics fix?

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Analytics />
       <body>
+        <Analytics />
         {children}
         <Footer />
       </body>

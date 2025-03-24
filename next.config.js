0668271ef9fe8598c4411702d23c58398d28b74c
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2025-03-24 13:12:10 +0100

    next rewrites

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/fordypningsprosjekt.php',
+                destination: 'https://www.idi.ntnu.no/education/fordypningsprosjekt.php',
+            },
+        ];
+    },
+};

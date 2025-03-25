49913b6e2c3a7f48fbf3d98dcf66f7574fc8fb45
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2025-03-25 09:41:32 +0100

    info styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -167,15 +167,15 @@ export default function ProjectBrowser() {
         </h1>
 
         <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-6">
-          <div className="flex">
+          <div className="flex items-center">
             <div className="flex-shrink-0">
-              <svg className="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+              <svg className="h-8 w-8 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
               </svg>
             </div>
             <div className="ml-3">
               <p className="text-sm text-yellow-700">
-                Project proposals can be registered <strong>from April 1, 2025</strong>. The deadline for submitting proposals are <strong>May 21, 2025</strong>.
+                Project proposals can be registered from <strong>April 1st</strong>.<br />The deadline for submitting proposals are <strong>May 21st</strong>.
               </p>
             </div>
           </div>

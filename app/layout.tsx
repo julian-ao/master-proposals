1e4247b93bd2852669504529c42077345b9f1868
Julian Ammouche Ottosen <42567826+julian-ao@users.noreply.github.com>
2025-03-29 00:56:31 +0100

    Update app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
                         <AISummaryProvider>
                             <ImprovedTitlesProvider>
                                 <Analytics />
-                                
                                 {children}
                                 <Footer />
                                 <Toaster />
